c16b9094568004042a67fa9dd6269f045800364c
Muhammet Halak <75750279+halak0013@users.noreply.github.com>
2024-09-26 11:24:49 +0300

    documentation

--- DrawingClass.py
+++ DrawingClass.py
@@ -8,9 +8,9 @@ from dk_connection import ImageReceiver, SocketServer
 # daha fazla renk siyah, kahverengi, turuncu, sarı, mor, pembe, beyaz
 # sitcker
 # seçimin ne olduğu
-# 
+
 class HandDrawingApp:
-    def __init__(self, target_width=1800, target_height=1000, detection_confidence=0.65, max_hands=1):
+    def __init__(self, target_width=1280, target_height=720, detection_confidence=0.40, max_hands=1, source="camera"):
         self.target_width = target_width
         self.target_height = target_height
         self.detection_confidence = detection_confidence
@@ -22,9 +22,15 @@ class HandDrawingApp:
         self.yp = 0
         self.first_time = True
 
-        self.cap = cv2.VideoCapture(0)
-        self.cap.set(3, self.target_width)
-        self.cap.set(4, self.target_height)
+        self.source = source
+
+        if source == "camera":
+            self.cap = cv2.VideoCapture(0)
+            self.cap.set(3, self.target_width)
+            self.cap.set(4, self.target_height)
+        else:
+            self.sock_server = SocketServer()
+            self.image_receiver = ImageReceiver(self.sock_server)
 
         # Load headers
         self.header_blue = cv2.imread(
@@ -64,7 +70,7 @@ class HandDrawingApp:
             self.brushes[i]["img"] = cv2.resize(
                 self.brushes[i]["img"], (self.target_width, 100))
 
-    def is_sellection_mode(self, hands):
+    def is_selection_mode(self, hands):
         """
         1 çizim modu\n
         2 seçim modu
@@ -72,7 +78,7 @@ class HandDrawingApp:
         finger_up = self.detector.fingersUp(hands[0])
         mode = ""
         if finger_up[1] and finger_up[2]:
-            mode = "sellection"
+            mode = "selection"
         elif finger_up[1]:
             mode = "drawing"
         else:
@@ -87,9 +93,11 @@ class HandDrawingApp:
             self.canvas_mask = np.zeros(
                 (self.target_height, self.target_width, 3), np.uint8)
 
-    def draw(self, hands):
+    def draw(self, hands, img: cv2.typing.MatLike):
         if hands:
             x, y, z = self.finger_position(hands)
+            cv2.circle(img, (x, y),
+                       10, self.brushes[self.brush_index]["color"], cv2.FILLED)
             if self.xp == 0 and self.yp == 0 or self.first_time:
                 self.xp, self.yp = x, y
             if self.brush_index == 3:
@@ -116,7 +124,7 @@ class HandDrawingApp:
         img = cv2.add(img, self.canvas_mask)
         return img
 
-    def sellecting(self, finger_pos):
+    def selecting(self, finger_pos):
         x = self.target_width / 100
         if 24 * x < finger_pos[0] < 31 * x and finger_pos[1] < 100:
             self.brush_index = 0
@@ -127,21 +135,21 @@ class HandDrawingApp:
         if 66 * x < finger_pos[0] < 71 * x and finger_pos[1] < 100:
             self.brush_index = 3
 
-    def get_img(self, source="camera"):
+    def get_img(self):
         img = None
-        succes = None
-        if source == "sock":
+        success = None
+        if self.source == "sock":
             img = self.image_receiver.get_image()
-            succes = img is not None
+            success = img is not None
         else:
-            succes, img = self.cap.read()
-        return succes, img
+            success, img = self.cap.read()
+        return success, img
 
     def run(self):
         while True:
-            success, img = self.get_img("sock")
+            success, img = self.get_img()
             if not success:
-                continue
+                break
             img = cv2.flip(img, 1)
             img = cv2.resize(img, (self.target_width, self.target_height))
             # Detect hands
@@ -152,11 +160,11 @@ class HandDrawingApp:
                 img, self.brushes[self.brush_index]["img"], [0, 0])
 
             if hands:
-                if self.is_sellection_mode(hands) == "drawing":
-                    self.draw(hands)
+                if self.is_selection_mode(hands) == "drawing":
+                    self.draw(hands,img)
                     self.first_time = False
-                elif self.is_sellection_mode(hands) == "sellection":
-                    self.sellecting(self.finger_position(hands))
+                elif self.is_selection_mode(hands) == "selection":
+                    self.selecting(self.finger_position(hands))
                     self.first_time = True
                 else:
                     self.first_time = True
@@ -166,7 +174,7 @@ class HandDrawingApp:
             # Overlay header
             img = cvzone.overlayPNG(
                 img, self.brushes[self.brush_index]["img"], [0, 0])
-            cv2.imshow("Deneyap", img)
+            cv2.imshow("Deneyap Ciz", img)
             # cv2.imshow("mask", self.canvas_mask)
             if cv2.waitKey(5) & 0xFF == 27:  # Press 'ESC' to exit
                 print("Çık")
@@ -177,5 +185,5 @@ class HandDrawingApp:
 
 
 if __name__ == "__main__":
-    app = HandDrawingApp()
+    app = HandDrawingApp(source="camera")
     app.run()
